f02b9543bf332f7befc73a1f1c367d46be14f0bb
Kai Koh <34813302+kaikoh95@users.noreply.github.com>
2018-08-21 20:35:46 +1200

    Update extras.py

--- extras.py
+++ extras.py
@@ -4,7 +4,7 @@ Extra classes built on search.py module.
 
 
 from search import *
-from itertools import dropwhile
+from itertools import *
 from math import *
 from heapq import *
 
@@ -65,6 +65,58 @@ class LCFSFrontier(Frontier):
     def __iter__(self):
         while self.container:
             yield heappop(self.container)[1]
+
+            
+class AStarFrontier(Frontier):
+    """Performs A* search on graphs. An instance of AStarFrontier together
+    with an instance of RoutingGraph will be passed to the generic search
+    procedure in order to find the lowest cost solution (if one exists),
+    from one of the agents to the goal node. Agents have infinite fuel.
+    
+    Multiple-path pruning is used by using a dictionary to keep track of points
+    visited and the fuel left.
+    
+    This algorithm halts on all valid maps even when there is no solution.
+    """    
+    
+    def __init__(self, map_graph):
+        
+        self.map_graph = map_graph
+        self.container = [] #initialised as empty
+        self.visited = {} #contains visited_points : fuel_left for backtrackking
+        self.steps_required = count() #using itertools
+
+    def add(self, path):
+        
+        curr_row, curr_col, curr_fuel = path[-1].head
+        pos = (curr_row, curr_col)
+        fuel_left = self.visited.get(pos)
+        if fuel_left is None or fuel_left < curr_fuel:
+            cost = 0
+            
+            for arc in path:
+                cost += arc.cost
+                
+            cost += self.map_graph.estimated_cost_to_goal(path[-1].head) #account for heuristic
+            heappush(self.container, (cost, next(self.steps_required), path))
+
+    def __iter__(self):
+
+        while self.container:
+            cost, steps_required, path = heappop(self.container)
+            row, col, fuel = path[-1].head
+            pos = (row, col)
+            fuel_left = self.visited.get(pos)
+            
+            if fuel_left is None:
+                self.visited[pos] = fuel
+                yield path
+                
+            else:
+                
+                if fuel_left < fuel:
+                    self.visited[pos] = fuel
+                    yield path                   
 
 
 class FunkyNumericGraph(Graph):
@@ -188,10 +240,154 @@ class LocationGraph(ExplicitGraph):
                 tail, head, cost = edge
             if tail == node:
                 yield Arc(tail, head, str(tail) + '->' + str(head), cost)  
-                
+     
 
-def main():
+class RoutingGraph(Graph):
+    """Initialised by the map string and represents the map
+    in the form of a graph by implementing all the required methods
+    in the Graph class, including the method estimated_cost_to_goal. 
+    Represent the state of the agent by a tuple of the form (row, column, fuel).
+    """
     
+    def __init__(self, map_str):
+        """Initialises the class and processes the map string given."""
+        
+        self.map1 =  map_str.splitlines()
+        self.starting_list = []
+        self.outgoing = []
+        
+        #Objects to declare
+        self.gaps = " "
+        self.obstacles = "+-|X"
+        self.fuel_up = "F"
+        self.agents = "S0123456789"
+        self.dest = "G"        
+
+        for row in range(len(self.map1)):
+            for col in range(len(self.map1[row])):
+                obj = self.map1[row][col]
+                
+                if obj in self.agents:
+                    if obj == 'S':
+                        fuel = inf
+                    else:
+                        fuel = int(obj)
+                    state = (row, col, fuel)
+                    self.starting_list.append(state)
+                elif obj in self.dest:
+                    self.goal_node = (row, col)
+    
+    def starting_nodes(self):
+        """Returns a list of starting nodes."""
+        
+        for nodes in self.starting_list:
+            yield nodes
+
+    def is_goal(self, node):
+        """Returns true if the given node is a goal node."""
+        
+        row, col, fuel = node
+        return (row, col) == self.goal_node
+    
+    def estimated_cost_to_goal(self, node):
+        """Gets the estimated cost to the goal node from a node.
+        Generates formula for most dominant (highest value) function 
+        that can be computed in constant time. (Manhattan Distance used).
+        """
+        
+        curr_row, curr_col, fuel = node
+        goal_row, goal_col = self.goal_node
+        row, col = abs(curr_row - goal_row), abs(curr_col - goal_col)
+        D, D2 = 2, 2
+        formula = D * max(row, col) + (D2 - D) * min(row, col)
+        
+        return formula
+    
+    def outgoing_arcs(self, tail):
+        """Gets the outgoing arcs for a node"""
+        
+        directions = [('N' , -1, 0),
+                      ('NE', -1, 1),
+                      ('E' ,  0, 1),
+                      ('SE',  1, 1),
+                      ('S' ,  1, 0),
+                      ('SW',  1, -1),
+                      ('W' ,  0, -1),
+                      ('NW', -1, -1)]  
+        
+        row, col, fuel = tail
+        original_pos = self.map1[row][col]
+        
+        if fuel > 0:
+            
+            for label, row_change, col_change in directions: 
+                new_row = row + row_change
+                new_col = col + col_change
+                new_pos = self.map1[new_row][new_col]
+                head = (new_row, new_col, fuel-1)
+                
+                if new_pos not in self.obstacles:
+                    cost = 2
+                    yield Arc(tail, head, label, cost)
+                    
+        if original_pos in self.fuel_up and fuel < 9:
+            fuel = 9
+            head = (row, col, fuel) #restores fuel to max capacity
+            label = 'Fuel up'
+            cost = 5
+            yield Arc(tail, head, label, cost)                           
+
+
+def print_map(map_graph, frontier, solution):
+    """Takes three parameters: an instance of RoutingGraph, 
+    a frontier object, and a solution (which is a sequence of Arc objects
+    that make up a path from a starting position to the goal position).
+    
+    Then prints a map such that:
+    - the position of the walls, obstacles, agents, and the goal point 
+      are all unchanged and they are marked by the same set of characters 
+      as in the original map string
+    - those free spaces (space characters) that have been expanded during 
+      the search are marked with a '.' (a period character)
+    - those free spaces (spaces characters) that are part of the solution
+      (best path to the goal) are marked with '*' (an asterisk character).
+    """
+    
+    '''
+    since python strings are immutable, have to
+    convert into nested list in order to replace
+    gaps with . or *
+    '''
+    map_list = []
+    row, col = 0, 0
+    for row in range(len(map_graph.map1)):
+        the_row = []
+        for col in range(len(map_graph.map1[row])):
+            the_row.append(map_graph.map1[row][col])
+        map_list.append(the_row)
+    
+    ptr = str(map_list[row][col]) #temp placeholder for ptr
+            
+    for pos in frontier.visited.keys():
+        row, col = pos
+        ptr = str(map_list[row][col])  
+        if ptr in map_graph.gaps:
+            map_list[row][col] = '.' #path explored but not taken
+    
+    if solution:
+        for route in solution:
+            row, col, fuel = route.head
+            ptr = str(map_list[row][col])
+            if ptr in map_graph.gaps or ptr == '.':
+                map_list[row][col] = '*' #path explored and taken
+    
+    for row in range(len(map_list)):
+        for col in range(len(map_list[row])):
+            print(map_list[row][col], end='')
+        print() 
+
+        
+def main():    
     """# DFS Frontier
     # Example 1
     graph = ExplicitGraph(nodes=set('SAG'),
@@ -332,6 +528,7 @@ def main():
     for arc in graph.outgoing_arcs('C'):
         print(arc)    
     """
+    """
     #LCFS Frontier 
     #Example 1
     graph = LocationGraph(nodes=set('ABC'),
@@ -387,9 +584,202 @@ def main():
     
     solution = next(generic_search(graph, LCFSFrontier()))
     print_actions(solution)
+    """
+    '''
+    #Routing Graph
+    #Example 1
+    map_str = """\
++-------+
+|  9  XG|
+|X XXX  |
+| S  0F |
++-------+
+"""
+
+    graph = RoutingGraph(map_str)
+
+    print("Starting nodes:", sorted(graph.starting_nodes()))
+    print("Outgoing arcs (available actions) at starting states:")
+    for s in sorted(graph.starting_nodes()):
+        print(s)
+        for arc in graph.outgoing_arcs(s):
+            print ("  " + str(arc))
+
+    node = (1,1,5)
+    print("\nIs {} goal?".format(node), graph.is_goal(node))
+    print("Outgoing arcs (available actions) at {}:".format(node))
+    for arc in graph.outgoing_arcs(node):
+        print ("  " + str(arc))
+
+    node = (1,7,2)
+    print("\nIs {} goal?".format(node), graph.is_goal(node))
+    print("Outgoing arcs (available actions) at {}:".format(node))
+    for arc in graph.outgoing_arcs(node):
+        print ("  " + str(arc))
+
+    node = (3,6,5)
+    print("\nIs {} goal?".format(node), graph.is_goal(node))
+    print("Outgoing arcs (available actions) at {}:".format(node))
+    for arc in graph.outgoing_arcs(node):
+        print ("  " + str(arc))
+
+    node = (3,6,9)
+    print("\nIs {} goal?".format(node), graph.is_goal(node))
+    print("Outgoing arcs (available actions) at {}:".format(node))
+    for arc in graph.outgoing_arcs(node):
+        print ("  " + str(arc))
+    #Example 2
+    map_str = """\
++--+
+|GS|
++--+
+"""
+
+    graph = RoutingGraph(map_str)
+
+    print("Starting nodes:", sorted(graph.starting_nodes()))
+    print("Outgoing arcs (available actions) at the start:")
+    for start in graph.starting_nodes():
+        for arc in graph.outgoing_arcs(start):
+            print ("  " + str(arc))
+
+
+
+    node = (1,1,1)
+    print("\nIs {} goal?".format(node), graph.is_goal(node))
+    print("Outgoing arcs (available actions) at {}:".format(node))
+    for arc in graph.outgoing_arcs(node):
+        print ("  " + str(arc))
+    #Example 3
+    map_str = """\
++----+
+| X  |
+|XSX |
+| X G|
++----+
+"""
+
+    graph = RoutingGraph(map_str)
+
+    print("Starting nodes:", sorted(graph.starting_nodes()))
+    print("Available actions at the start:")
+    for s in graph.starting_nodes():
+        for arc in graph.outgoing_arcs(s):
+            print ("  " + arc.label)
+    '''
+    '''
+    #A* Routing Frontier
+    #Example 1
+    map_str = """\
++-------+
+|  F  XG|
+|X XXXX |
+| 2     |
++-------+
+"""
+
+    map_graph = RoutingGraph(map_str)
+    frontier = AStarFrontier(map_graph)
+    solution = next(generic_search(map_graph, frontier), None)
+    print_actions(solution)
+    #Example 2
+    map_str = """\
++---------+
+|         |
+|    G    |
+|         |
++---------+
+"""
+
+    map_graph = RoutingGraph(map_str)
+    frontier = AStarFrontier(map_graph)
+    solution = next(generic_search(map_graph, frontier), None)
+    print_actions(solution)
+    #Example 3
+    map_str = """\
++----------------+
+|2             F |
+|XX   G 123      |
+|2XXXXXXXXXXXXXX |
+|  F             |
+|           F    |
++----------------+
+"""
+
+    map_graph = RoutingGraph(map_str)
+    frontier = AStarFrontier(map_graph)
+    solution = next(generic_search(map_graph, frontier), None)
+    print_actions(solution)
+    '''
+    '''
+    #Map Visualization
+    #Example 1
+    map_str = """\
++------------+
+|            |
+|            |
+|            |
+|    S       |
+|            |
+|            |
+| G          |
+|            |
++------------+
+"""
+
+    map_graph = RoutingGraph(map_str)
+    frontier = AStarFrontier(map_graph)
+    solution = next(generic_search(map_graph, frontier), None)
+    print_map(map_graph, frontier, solution)
+    #Example 2
+    map_str = """\
++---------------+
+|    G          |
+|XXXXXXXXXXXX   |
+|           X   |
+|  XXXXXX   X   |
+|  X S  X   X   |
+|  X        X   |
+|  XXXXXXXXXX   |
+|               |
++---------------+
+"""
+
+    map_graph = RoutingGraph(map_str)
+    frontier = AStarFrontier(map_graph)
+    solution = next(generic_search(map_graph, frontier), None)
+    print_map(map_graph, frontier, solution)
+    #Example 3
+    map_str = """\
++-------------+
+|         G   |
+| S           |
+|         S   |
++-------------+
+"""
+
+    map_graph = RoutingGraph(map_str)
+    frontier = AStarFrontier(map_graph)
+    solution = next(generic_search(map_graph, frontier), None)
+    print_map(map_graph, frontier, solution)
+    #Example 4
+    map_str = """\
++------------+
+|         X  |
+| S       X G|
+|         X  |
+|         X  |
+|         X  |
++------------+
+"""
+
+    map_graph = RoutingGraph(map_str)
+    frontier = AStarFrontier(map_graph)
+    solution = next(generic_search(map_graph, frontier), None)
+    print_map(map_graph, frontier, solution)
+    '''
+            
     
     
-        
 if __name__ == "__main__":
     main()
-            
