3729fe166938a60bacafadf9c03829b2f1739f49
Kai Koh <34813302+kaikoh95@users.noreply.github.com>
2018-09-24 15:05:30 +1200

    Create alarm_network.py

---
+++ alarm_network.py
@@ -0,0 +1,39 @@
+"""
+Representation of the alarm network for Belief Networks and Probabilistic Inference.
+"""
+
+alarm_net = {
+    'Burglary': {
+        'Parents': [],
+        'CPT': {
+            (): 0.001
+            }},
+            
+    'Earthquake': {
+        'Parents': [],
+        'CPT': {
+            (): 0.002,
+            }},
+    'Alarm': {
+        'Parents': ['Burglary','Earthquake'],
+        'CPT': {
+            (True,True): 0.95,
+            (True,False): 0.94,
+            (False,True): 0.29,
+            (False,False): 0.001,
+            }},
+
+    'John': {
+        'Parents': ['Alarm'],
+        'CPT': {
+            (True,): 0.9,
+            (False,): 0.05,
+            }},
+
+    'Mary': {
+        'Parents': ['Alarm'],
+        'CPT': {
+            (True,): 0.7,
+            (False,): 0.01,
+            }},
+    }
