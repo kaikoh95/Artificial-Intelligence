3478d2da7fe14699b6bdabdd5ce08a7213830ccc
Kai Koh <34813302+kaikoh95@users.noreply.github.com>
2018-10-03 12:36:12 +1300

    Update alarm_network.py

--- alarm_network.py
+++ alarm_network.py
@@ -2,6 +2,56 @@
 Representation of the alarm network for Belief Networks and Probabilistic Inference.
 """
 
+import itertools
+
+
+def joint_prob(network, assignment):
+    probability = 1
+
+    for i in assignment.keys():
+        value = network[i]
+        get_parents = []
+
+        for j in value['Parents']:
+            get_parents.append(assignment[j])
+
+        parents = tuple(get_parents)
+        result = value['CPT'].get(parents)
+        sol = 0
+        if assignment[i] is True:
+            sol = result
+        else:
+            sol = 1 - result
+
+        probability *= sol
+
+    return probability
+
+
+def query(network, query_var, evidence):
+    hidden_vars = network.keys() - evidence.keys() - {query_var}  # finds set of hidden variables
+    hidden_assignments = {}
+    query_true, query_false = 0, 0
+
+    for values in itertools.product((True, False), repeat=len(hidden_vars)):  # obtain all possible hidden assignments
+        hidden_assignments = {var: val for var, val in zip(hidden_vars, values)}
+        hidden_assignments.update(evidence)
+
+        # solve for False
+        hidden_assignments[query_var] = False
+        query_false += joint_prob(network, hidden_assignments)
+
+        # solve for True
+        hidden_assignments[query_var] = True
+        query_true += joint_prob(network, hidden_assignments)
+
+    query_main = query_false + query_true
+    false_query = query_false / query_main
+    true_query = query_true / query_main
+
+    return false_query, true_query
+
+
 alarm_net = {
     'Burglary': {
         'Parents': [],
